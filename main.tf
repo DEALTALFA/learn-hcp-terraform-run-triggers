bf68f7d838776bb3f1815d14e04be7f201d9ea70
Deepak Yadav <60976631+DEALTALFA@users.noreply.github.com>
2026-02-10 18:14:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = data.tfe_outputs.source_workspace.nonsensitive_values.instance_security_group_ids
-  subnet_id              = data.tfe_outputs.source_workspace.nonsensitive_values.instance_subnet
+  vpc_security_group_ids = "sg-997301bd"
+  subnet_id              = "subnet-5f7ed72"
 
   tags = {
     Name = var.instance_name
